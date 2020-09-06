7f689e14025ff75149737ada181c80f5f5c0d712
AmrAhmedd <amr.aboelnaga@student.guc.edu.eg>
2020-09-06 22:50:04 +0200

    deploy not working

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
           <Route
             path="/visualization/sorting"
             component={HeapBlock}
-
+            exact
           />
           <Route
             path="/pathfinding"
